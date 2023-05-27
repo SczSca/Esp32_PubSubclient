6516e72bea5d471e028b693f179a32cbda8048f1
SczSca <aalvarez59@ucol.mx>
2023-05-27 11:58:51 -0600

    servidor websocket

--- app/websocket/websocketServer.js
+++ app/websocket/websocketServer.js
@@ -1,38 +1,70 @@
 const WebSocket = require('ws');
+const {WS_BROKERID} = require('../config/env/config.js');
 
 //se subscribe como subscriptor mqtt 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8081 });
 
 // Almacenar las conexiones de los clientes
 const clients = new Set();
 
+let clientBroker;
+
 wss.on('connection', (ws) => {
   // Agregar nuevo cliente a la lista
   clients.add(ws);
-
-  ws.on('message', (message) => {
+  console.log("Client connected")
+  ws.on('message', (message, isBinary) => {
     // Enviar el mensaje a todos los clientes conectados
     /**
      * jsonMessage {
      * Buffer: valor,
-     * data: [arr de bytes]
+     * data: [arr de bytes] ## mensaje del ws cliente recibido
      * }
      */
     const jsonMessage = JSON.stringify(message)
     /**se obtiene la data del obj json message, se convierte de bytes a string y ese string se convierte a json
-     * jsonData {
-     * type: string,
-     * name: string,
-     * data: string [solo para el type === "message"]
+       jsonData { //json que recibe el broker por mqtt y lo manda a los ws
+        target: string,
+        temperatura: {
+                data: float,
+                bulb: bool [0 || 1]
+            },
+            movimiento: {
+                data: float,
+                bulb: bool [0 || 1]
+            },
+            sonico: {
+                data: float,
+                bulb: bool [0 || 1]
+            },
+            uv: {
+                data: float,
+                bulb: bool [0 || 1]
+            }
+        }
+
+        jsonData { //json que manda el broker al wss para avisarle que el será el cliente ws broker
+            broker: string [id del broker]
+        }
+
+        jsonData { //json que manda al ws broker para que se lo mande al esp32
+            target: string, [clientBroker]
+            sensor: string, [temperatura || movimiento || sonico || uv]
+            bulb: bool [0 || 1]
+        }
      */
     const jsonData = JSON.parse(String.fromCharCode(...JSON.parse(jsonMessage).data))
-    if (jsonData.type === 'message' || jsonData.type === 'login' || jsonData.type === 'exit'){
+    if(jsonData.target === 'clientBroker') {
+        sendToClientBroker(JSON.stringify(jsonData), ws);       
+    }else if(jsonData.target === 'broadcast'){
         broadcast(JSON.stringify(jsonData));
+    }else if(jsonData.broker == WS_BROKERID){
+        clientBroker = ws;
+        console.log('Client broker connected');        
     }else{
-        console.error(`Error: Invalid type "${jsonData.type}"`);
+        console.error(`Error: tipo de mensaje no valido : ${JSON.stringify(jsonData)}`);
+
     }
-    console.log(JSON.parse(String.fromCharCode(...JSON.parse(jsonMessage).data)));
-    
   });
 
   ws.on('close', () => {
@@ -43,9 +75,24 @@ wss.on('connection', (ws) => {
 });
 
 function broadcast(message, isBinary) {
-  // Enviar el mensaje a todos los clientes conectados
+  // Enviar el mensaje a todos los clientes conectados excepto el mensajero
   clients.forEach((client) => {
-    client.send(message, { binary: isBinary});
+    if(client !== clientBroker && client.readyState == WebSocket.OPEN){
+        client.send(message, { binary: isBinary});
+    }
     
   });
+}
+
+//Función que se encarga de que los clientes ws se comuniquen con el cliente ws broker
+function sendToClientBroker(message, ws, isBinary) {
+    if(clientBroker && clientBroker.readyState == WebSocket.OPEN){
+        clientBroker.send(message, { binary: isBinary});
+    }else{ //si no se encuentra el broker conectado, se le manda json al remitente
+        const error = {
+            type: 'error',
+            message: 'Client broker not connected'
+        }
+        ws.send(JSON.stringify(error));
+    }
 }
