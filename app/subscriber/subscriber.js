32a46eae15f3e5be3c305a523ab9b5acd25eaef0
SczSca <aalvarez59@ucol.mx>
2023-05-27 12:12:51 -0600

    archivo que activa la conexión a mqtt y wss

--- app/subscriber/subscriber.js
+++ app/subscriber/subscriber.js
@@ -1,23 +1,7 @@
-
-const {subscribeToTopic} = require('../controllers/SubscriberController.js');
 const {connectToBroker} = require('../config/use_ws.js');
-// import * as subscribeController from '../controllers/SubscriberController.mjs';
-// import * as brokerModule from '../config/use_ws.mjs';
-// const connectToBroker = brokerModule.connectToBroker;
-// connectToBroker();
-// window.addEventListener('load', (e) =>{
-//     connectToBroker('mqttSubscriber');
-//     console.log("load")
-//     console.log(1)
-//     subscribeToTopic();
-//     // subscribeController.subscribeToTopic();
-// })
-// window.onload = (event) => {
-//     console.log("load")
-// }
+
+/**
+ * Cliente websocket mensajero que mandara los datos mqtt a todos los demás clientes websocket
+ */
 
 connectToBroker('mqttSubscriber');
-// console.log(1)
-subscribeToTopic();
-//p
-// console.log(2)
